fa1cf005f033bc864c26cf897fc8ad9a4180546f
Alain Kaiser <kaiseralain1@gmail.com>
2023-09-07 09:39:50 +0200

    wip: finished basic example

--- 00_basics.js
+++ 00_basics.js
@@ -3,23 +3,29 @@ import OpenAI from "openai";
 
 config();
 
+// OpenAI configuration
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// Chat helper function to send a message to the API
 async function chat(input) {
   const messages = [{ role: "user", content: input }];
 
   const response = await openai.chat.completions.create({
-    messages: messages,
     model: "gpt-3.5-turbo",
+    messages: messages,
+    /* 
+    Value from 0 to 1 - Lower temperature results in more consistent outputs
+    Higher temperature results in more diverse and creative results
+    */
     temperature: 0,
   });
 
   return response.choices[0].message.content;
 }
 
-const question = "What is the capital of France";
+const question = "What is an LLM (large language model)?";
 
 try {
   const response = await chat(question);
