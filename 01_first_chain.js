713f7cf8fa137801b5f044b675f56e756ffb3732
Alain Kaiser <kaiseralain1@gmail.com>
2023-09-11 11:26:45 +0200

    added comments for 01_first_chain

--- 01_first_chain.js
+++ 01_first_chain.js
@@ -6,14 +6,20 @@ import prompts from "prompts";
 
 config();
 
+/*
+  The basic building block of LangChain is the LLM, which takes in text and generates some more text. As an example, suppose we're building an application that generates a company name based on a company which makes products. In order to do this, we need to initialize an OpenAI model wrapper. In this case, since we want the outputs to be MORE random, we'll initialize our model with a HIGH temperature.
+*/
+
 const llm = new OpenAI({
   temperature: 0.9,
 });
 
+// Create a prompt template with a placeholder for the product name (which will be filled in by the user)
 const prompt = PromptTemplate.fromTemplate(
   "What is a good name for a company that makes {product}?"
 );
 
+// Create a chain with the LLM and prompt template
 const chain = new LLMChain({ llm, prompt });
 
 (async () => {
