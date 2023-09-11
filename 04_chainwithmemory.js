e76ccae467e5691b7431a34de959d59f60702b3d
Alain Kaiser <kaiseralain1@gmail.com>
2023-09-11 13:41:36 +0200

    added comments for 04_chainwithmemory

--- 04_chainwithmemory.js
+++ 04_chainwithmemory.js
@@ -5,15 +5,24 @@ import { ConversationChain } from "langchain/chains";
 
 config();
 
-const model = new OpenAI({});
+/*
+  In this example, we will create a chain that is capable of remembering information from previous conversations. This is useful if you want to create a chatbot that can remember information from previous conversations.
+ */
+
+// We will start by initializing the llm and an instance of buffer memory
+const llm = new OpenAI({});
 const memory = new BufferMemory();
 
+/* 
+  Then, we will create a special chain, a so called "ConversationChain". This type of chain supports memory and will automatically store and retrieve information from the memory. It is useful when you want to create a conversation between a human and an AI.
+*/
 const chain = new ConversationChain({
-  llm: model,
+  llm,
   memory,
   verbose: true,
 });
 
+// Now we call the chain and give it an input
 const { response } = await chain.call({
   input: "Hi! I'm Alain, 27 years old from Switzerland",
 });
